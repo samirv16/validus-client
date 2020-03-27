15e0e197670d053239a25ec3b35566263ad3cf52
samirv16 <samir.vallecillo@outlook.com>
2020-03-27 14:36:31 -0500

    creates Home class

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './Home.css'
 
-const Home = () => {
-    return (
-        <div>
-            <h1>Home!</h1>
-        </div>
-    )
+class Home extends React.Component {
+    render() {
+        return <h1>Home!</h1>
+    }
 }
 
 export default Home
