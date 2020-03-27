7469762716ef7982647f7a1bc50a45d6e5c9d157
samirv16 <samir.vallecillo@outlook.com>
2020-03-27 11:36:50 -0500

    creates basic styling for links

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    backgroud: 'white',
+    textDecoration: 'none',
+    color: 'black',
+}
 
 export dafault Navbar
