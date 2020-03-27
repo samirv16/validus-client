40b10fee32ba69758dab936f60d89770ac2ee757
samirv16 <samir.vallecillo@outlook.com>
2020-03-27 14:31:54 -0500

    creates simple function for testing

--- src/containers/WorkoutForm.js
+++ src/containers/WorkoutForm.js
@@ -1,3 +1,11 @@
 import React from 'react'
 
+const WorkoutForm = () => {
+    return (
+        <div>
+            <h1>create your workout</h1>
+        </div>
+    )
+}
+
 export default WorkoutForm
--- src/containers/Workouts.js
+++ src/containers/Workouts.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Workouts = () => {
     return (
         <div>
-            <h1>Home!</h1>
+            <h1>See Workouts below!</h1>
         </div>
     )
 }
