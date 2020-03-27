3ac8f64e4ac71c2b3a1561ee326e70b4faeeff2c
samirv16 <samir.vallecillo@outlook.com>
2020-03-27 11:11:35 -0500

    creates home.css file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-import App from './App'
+import App from './containers/App'
 
 const store = createStore(applyMiddleware(thunk))
 
