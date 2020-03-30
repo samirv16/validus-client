6d40c69efbc0c9f2714181cf907aeb8e83d3b66a
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 18:33:58 -0500

    adds case statements for reducer

--- src/containers/reducers/workoutsReducer.js
+++ src/containers/reducers/workoutsReducer.js
@@ -1,7 +1,11 @@
 let id = 0
 
 export default(state= [], action) => {
+
     switch(action.type) {
+        case "ADD_WORKOUT":
+            
+        case "REMOVE_WOKOUT":
 
         default:
             return state
