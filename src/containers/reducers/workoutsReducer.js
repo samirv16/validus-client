25f0e541a79cf24cf10646ca2b5ee4433c41e553
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 18:45:18 -0500

    add workout adds payload to the end of state

--- src/containers/reducers/workoutsReducer.js
+++ src/containers/reducers/workoutsReducer.js
@@ -4,9 +4,9 @@ export default(state= [], action) => {
 
     switch(action.type) {
         case "ADD_WORKOUT":
-            
+            return [...state, action.payload]
         case "REMOVE_WOKOUT":
-
+            
         default:
             return state
     }
