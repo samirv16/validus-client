c3ae321a473ef09d02a76faa6327ed5487504415
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 09:54:10 -0500

    moves app file to src dir

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Home from './Home'
-import Workouts from './Workouts'
-import WorkoutForm from './WorkoutForm'
-import About from './About'
-import Navbar from '../components/Navbar'
+import Home from './containers/Home'
+import Workouts from './containers/Workouts'
+import WorkoutForm from './containers/WorkoutForm'
+import About from './containers/About'
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
