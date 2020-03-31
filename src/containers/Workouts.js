5b8f31fa2b8b5d9fe3b664beb0569c28985da3a3
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 19:45:01 -0500

    makes unordered workout list

--- src/containers/Workouts.js
+++ src/containers/Workouts.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import WorkoutItem from '../components/WorkoutItem'
 import {connect} from 'react-redux'
 
-
+export class Workouts extends Component {
+    render() {
+        const workouts = this.props.workouts.map(( workout, i) => <WorkoutItem key={i} workout={workout} />)
+        return (
+            <div>
+                <h2>Workout List</h2>
+                <ul>
+                    {workouts}
+                </ul>
+            </div>
+        )
+    }
+}
 
 export default Workouts
