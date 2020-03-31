c7a4c8abb2c234dcb9d3801dddefe670eded6341
samirv16 <samir.vallecillo@outlook.com>
2020-03-31 13:06:57 -0500

    starts fetch request

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,16 @@ export const addWorkout = workout => {
         type: "ADD_WORKOUT", payload:workout
     }
 }
+
+export const removeWorkout = workoutId => {
+    return {
+        type: "REMOVE_WORKOUT", payload:workoutId
+    }
+}
+
+export const fetchWorkouts = () = {
+    return (dispatch) => {
+        return fetch('http://localhost:3000/pets')
+        .then(resp => resp.json())
+    }
+}
