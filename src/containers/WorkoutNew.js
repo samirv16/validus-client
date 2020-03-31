22bb9ebe852a9ca029a7a06b4341e78afc83edff
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 20:03:37 -0500

    builds constructor and state

--- src/containers/WorkoutNew.js
+++ src/containers/WorkoutNew.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {addWorkout} from '../actions/index'
 import {connect} from 'react-redux'
 
+export class WorkoutNew extends Component [
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: '',
+            body: '',
+        }
+    }
+]
 
 export default WorkoutNew
