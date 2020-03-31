1326e7179d9b789e4706bc261d64cdf5b1490478
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 19:54:57 -0500

    deletes current function

--- src/containers/WorkoutNew.js
+++ src/containers/WorkoutNew.js
@@ -3,12 +3,5 @@ import {addWorkout} from '../actions/index'
 import {connect} from 'react-redux'
 
 
-const WorkoutNew = () => {
-    return (
-        <div className='formContainer'>
-            <h1>create your workout</h1>
-        </div>
-    )
-}
 
 export default WorkoutNew
