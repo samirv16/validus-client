4f5925dfa659b42c0d2cdf5fd804da79d17a015a
samirv16 <samir.vallecillo@outlook.com>
2020-03-30 19:04:29 -0500

    a new payload = a new id

--- src/reducers/workoutsReducer.js
+++ src/reducers/workoutsReducer.js
@@ -1,10 +1,16 @@
 let id = 0
 
 export default(state= [], action) => {
+    const newWorkout = () => {
+        return {
+            ...action.payload,
+          id: id++  
+        }
+    }
 
     switch(action.type) {
         case "ADD_WORKOUT":
-            return [...state, action.payload]
+            return [...state, newWorkout]
         case "REMOVE_WOKOUT":
             let newWorkouts = state.filter(workout => workout.id !== action.payload)
             return [...newWorkouts]
