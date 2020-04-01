9daad70f6b33034fddb18d29c12c3c79420330f5
samirv16 <samir.vallecillo@outlook.com>
2020-04-01 18:01:16 -0500

    adds about text to container

--- src/containers/About.js
+++ src/containers/About.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 class About extends React.Component {
     render() {
-        return <h1>About Us</h1>
+        return (
+        <div>
+            <h1>About Us</h1>
+            <h3>
+            There are times in life getting to the gym isn't realistic. 
+            We all have busy schedules and understand the importance of quick and effective workouts that can easily be added to your day. 
+            That is why we have developed a free solution to manage your workouts at home.
+            Instead of repeating the same workout videos, you can manage your own variety of workouts using our simple to use website.
+            We believe Validus (Latin word meaning strong), will not only help you gain physical strength, 
+            but give you a strong grip on a manageable and unique at home fitness routine.
+            </h3>
+        </div>
+        )
     }
 }
 
