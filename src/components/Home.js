348c427c1bd5ce74151920f02286c214bcf031f3
samirv16 <samir.vallecillo@outlook.com>
2020-04-03 12:01:11 -0500

    changes home from class component to presentational

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,10 @@ const link = {
 }
 
 
-class Home extends React.Component {
-    render() {
-        return (
+const Home = () => 
         <div style={link} >
             <h1 style={{fontSize: 50}}>Welcome to Validus!</h1>
         </div>
 
-        )
-    }
-}
 
 export default Home
